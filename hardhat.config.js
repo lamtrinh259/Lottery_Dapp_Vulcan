e1b6cb3bacf3123569e1f41e67ef93e2e5ae1480
Lam Trinh <lam.trinh@global.t-bird.edu>
2023-01-18 01:38:50 +0700

    Added hardhat config.js file

--- hardhat.config.js
+++ hardhat.config.js
@@ -55,7 +55,8 @@ module.exports = {
   },
   namedAccounts: {
     deployer: {
-      default: 0,
+      default: 0, // here this will by default take the first account as deployer
+      0: 1, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
     player: {
       default: 1
